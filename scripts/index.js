309d19aa52d983e65d45de1d5a82cf58819de661
13ruce1337 <tagcoop@gmail.com>
2014-04-27 00:53:36 -0400

    added auto scroll

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,22 @@ $(function() {
       scrollDown = $('.scroll_down'),
       itemHeight = $('.posts li').height();
 
+  function autoScroll() {
+    if($('.content').scrollTop()+$('.post_info').height() > $('.content').height()) {
+      $('.content').animate({scrollTop: 0});
+    } else {
+      $('.content').animate({scrollTop: itemHeight+'px'});
+    }
+  }
+
+  setInterval(autoScroll, 7000);
+
   scrollDown.click(function() {
     $('.content').animate({scrollTop: itemHeight+'px'});
+    autoScroll();
   });
   scrollUp.click(function() {
     $('.content').animate({scrollTop: -itemHeight+'px'});
+    autoScroll();
   });
 });
